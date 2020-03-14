de69292e567096b9c61042100cc0e5c3d8d1e7b9
Derrik Campbell <derrikcampbell@hotmail.com>
2020-03-13 23:16:57 -0400

    setup files

---
+++ js/components/UserComponent.js
@@ -0,0 +1,36 @@
+export default {
+    props: ['liveuser'],
+
+    template: `
+    <div class="col-xs-12 col-sm-6 col-md-4 mx-auto">
+        <div class="card rounded" @click="navToUserHome()">
+            <div class="card-body text-center">
+                <img :src="'images/user/' + liveuser.avatar + '.jpg'" class="rounded-circle img-fluid">
+                <p>{{ liveuser.uname }}</p>
+            </div>
+        </div>
+        
+    
+    </div>`,
+
+    created: function() {
+        if(this.liveuser.avatar === null || this.liveuser.avatar === "null") {
+            this.liveuser.avatar = "temp_avatar";
+        }
+    },
+
+    methods: {
+        navToUserHome() {
+            debugger;
+
+            //send this user to its homepage and pass the user object to the homepage
+            this.$router.push({ name: "home", params: { currentuser: this.liveuser }})
+        }
+    }
+
+}
+
+
+
+
+
